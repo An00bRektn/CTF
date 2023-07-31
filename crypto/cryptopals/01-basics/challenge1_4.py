f6a8bdf61273a692e20f6967b3224a3acbae4433
An00bRektn <ryan.syed1305@gmail.com>
2023-07-30 22:52:49 -0500

    Technically an upsolve but I was basically there, didn't have enough time :p

--- crypto/cryptopals/01-basics/challenge1_4.py
+++ crypto/cryptopals/01-basics/challenge1_4.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # frequency taken from http://en.wikipedia.org/wiki/Letter_frequency
-englishLetterFreq = {'E': 12.70, 'T': 9.06, 'A': 8.17, 'O': 7.51, 'I': 6.97, 'N': 6.75, 'S': 6.33, 'H': 6.09, 'R': 5.99, 'D': 4.25, 'L': 4.03, 'C': 2.78, 'U': 2.76, 'M': 2.41, 'W': 2.36, 'F': 2.23, 'G': 2.02, 'Y': 1.97, 'P': 1.93, 'B': 1.29, 'V': 0.98, 'K': 0.77, 'J': 0.15, 'X': 0.15, 'Q': 0.10, 'Z': 0.07}
+english_freq = {'E': 12.70, 'T': 9.06, 'A': 8.17, 'O': 7.51, 'I': 6.97, 'N': 6.75, 'S': 6.33, 'H': 6.09, 'R': 5.99, 'D': 4.25, 'L': 4.03, 'C': 2.78, 'U': 2.76, 'M': 2.41, 'W': 2.36, 'F': 2.23, 'G': 2.02, 'Y': 1.97, 'P': 1.93, 'B': 1.29, 'V': 0.98, 'K': 0.77, 'J': 0.15, 'X': 0.15, 'Q': 0.10, 'Z': 0.07}
 ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 EXTRA_CHARS = ',.; _{}&!?@' 
 
@@ -24,7 +24,7 @@ def naive_freq_analysis(text):
         for letter, count in frequencies.items():
             observed = 100 * count / total
             if letter != 'BAD':
-                score += abs(englishLetterFreq[letter] - observed)
+                score += abs(english_freq[letter] - observed)
         return score * 0.5 * frequencies['BAD']
     except:
         return -1
---
+++ live_events/corctf_23/solve.py
@@ -0,0 +1,51 @@
+import random
+
+def random_alphastring(size):
+    return "".join(random.choices(alphabet, k=size))
+
+def add_key(key, block):
+    ct_idxs = [(k_a + pt_a) % len(alphabet) for k_a, pt_a in zip([alphabet.index(k) for k in key], [alphabet.index(pt) for pt in block])]
+    return "".join([alphabet[idx] for idx in ct_idxs])
+
+def remove_key(key, block):
+    ct_idxs = [(pt_a - k_a) % len(alphabet) for k_a, pt_a in zip([alphabet.index(k) for k in key], [alphabet.index(pt) for pt in block])]
+    return "".join([alphabet[idx] for idx in ct_idxs])
+
+def cbc(key, plaintext):
+    klen = len(key)
+    plaintext = pad(klen, plaintext)
+    iv = random_alphastring(klen)
+    blocks = [plaintext[i:i+klen] for i in range(0, len(plaintext), klen)]
+    prev_block = iv
+    ciphertext = ""
+    for block in blocks:
+        block = add_key(prev_block, block)
+        prev_block = add_key(key, block)
+        ciphertext += prev_block
+    return iv, ciphertext
+    
+def pad(block_size, plaintext):
+    plaintext += "X" * (-len(plaintext) % block_size)
+    return plaintext
+
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+bs = 16
+iv = 'RLNZXWHLULXRLTNP'
+ct = 'ZQTJIHLVWMPBYIFRQBUBUESOOVCJHXXLXDKPBQCUXWGJDHJPQTHXFQIQMBXNVOIPJBRHJQOMBMNJSYCRAHQBPBSMMJWJKTPRAUYZVZTHKTPUAPGAIJPMZZZDZYGDTKFLWAQTSKASXNDRRQQDJVBREUXFULWGNSIINOYULFXLDNMGWWVSCEIORQESVPFNMWZKPIYMYVFHTSRDJWQBTWHCURSBPUKKPWIGXERMPXCHSZKYMFLPIAHKTXOROOJHUCSGINWYEILFIZUSNRVRBHVCJPVPSEGUSYOAMXKSUKSWSOJTYYCMEHEUNPJAYXXJWESEWNSCXBPCCIZNGOVFRTGKYHVSZYFNRDOVPNWEDDJYITHJUBVMWDNNNZCLIPOSFLNDDWYXMYVCEOHZSNDUXPIBKUJIJEYOETXWOJNFQAHQOVTRRXDCGHSYNDYMYWVGKCCYOBDTZZEQQEFGSPJJIAAWVDXFGPJKQJCZMTPMFZDVRMEGMPUEMOUVGJXXBRFCCCRVTUXYTTORMSQBLZUEHLYRNJAAIVCRFSHLLPOANFKGRWBYVSOBLCTDAUDVMMHYSYCDZTBXTDARWRTAFTCVSDRVEENLHOHWBOPYLMSDVOZRLENWEKGAWWCNLOKMKFWWAZJJPFDSVUJFCODFYIMZNZTMAFJHNLNMRMLQRTJJXJCLMQZMOFOGFPXBUTOBXUCWMORVUIIXELTVIYBLPEKKOXYUBNQONZLPMGWMGRZXNNJBUWBEFNVXUIAEGYKQSLYSDTGWODRMDBHKCJVWBNJFTNHEWGOZFEZMTRBLHCMHIFLDLORMVMOOHGXJQIIYHZFMROGUUOMXBTFMKERCTYXFIHVNFWWIUFTGLCKPJRFDRWDXIKLJJLNTWNQIOFWSIUQXMFFVIIUCDEDFEJNLKLQBALRKEYWSHESUJJXSHYWNRNPXCFUEFRJKSIGXHFTKNJXSYVITDOGYIKGJIOOHUFILWYRBTCQPRPNOKFKROTFZNOCZXZEYUNWJZDPJDGIZLWBBDGZJNRQRPFFGOTGFBACCRKLAPFLOGVYFXVIIJMBBMXWJGLPOQQHMNBCINRGZRBVSMLKOAFGYRUDOPCCULRBE'
+
+ct_blocks = [ct[i:i+bs] for i in range(0, len(ct), bs)]
+
+unchained = [remove_key(chain, ct) for chain,ct in zip([iv] + ct_blocks[:-1], ct_blocks)]
+print("PASTE THIS INTO https://www.boxentriq.com/code-breaking/vigenere-cipher#tool")
+print("".join(unchained))
+
+key = "acxqtstcsxzwfczy".upper()
+
+plaintext = []
+for chain,ct in zip([iv] + ct_blocks[:-1], ct_blocks):
+    block = remove_key(chain, remove_key(key, ct))
+    plaintext.append(block)
+
+pt = "".join(plaintext)
+print(pt)
+print(f"corctf{{{pt[-47:]}}}")
